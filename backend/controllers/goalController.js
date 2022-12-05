e6c4d3a7c64c847d658dd2784460f747062c927f
Paul Thomas <paulthomas2107@gmail.com>
2022-12-05 18:23:24 +0000

    More....

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,18 +1,27 @@
-// @desc    Get Goals
-// @route   Get /api/goals
+// @desc    getGoal
+// @route   GET /api/goals
 // @access  Private
 const getGoal = (req, res) => {
   res.status(200).json({ message: 'Get Goal' });
 };
 
+// @desc    setGoal
+// @route   POST /api/goals
+// @access  Private
 const setGoal = (req, res) => {
   res.status(200).json({ message: 'Set Goal' });
 };
 
+// @desc    updateGoal
+// @route   PUT /api/goals/:id
+// @access  Private
 const updateGoal = (req, res) => {
   res.status(200).json({ message: `Update Goal ${req.params.id}` });
 };
 
+// @desc    deleteGoal
+// @route   DELETE /api/goals/:id
+// @access  Private
 const deleteGoal = (req, res) => {
   res.status(200).json({ message: `Delete Goal ${req.params.id}` });
 };
