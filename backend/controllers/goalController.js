7fe48fe5b3d29d969726d8ce47083af6fac06963
Paul Thomas <paulthomas2107@gmail.com>
2022-12-05 19:11:16 +0000

    PreDB

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc    getGoal
 // @route   GET /api/goals
 // @access  Private
-const getGoal = (req, res) => {
+const getGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get Goal' });
-};
+});
 
 // @desc    setGoal
 // @route   POST /api/goals
 // @access  Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error('Please add a text field.');
   }
 
   res.status(200).json({ message: 'Set Goal' });
-};
+});
 
 // @desc    updateGoal
 // @route   PUT /api/goals/:id
 // @access  Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Goal ${req.params.id}` });
-};
+});
 
 // @desc    deleteGoal
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete Goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoal,
