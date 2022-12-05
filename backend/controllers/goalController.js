bb78760d8bad9705a2a7cc2f5ba6f3bb0e3ef24a
Paul Thomas <paulthomas2107@gmail.com>
2022-12-05 17:28:37 +0000

    Rework

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,25 @@
+// @desc    Get Goals
+// @route   Get /api/goals
+// @access  Private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: 'Get Goals' });
+};
+
+const setGoals = (req, res) => {
+  res.status(200).json({ message: 'Set Goal' });
+};
+
+const updateGoals = (req, res) => {
+  res.status(200).json({ message: `Update Goal ${req.params.id}` });
+};
+
+const deleteGoals = (req, res) => {
+  res.status(200).json({ message: `Delete Goal ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoals,
+  updateGoals,
+  deleteGoals,
+};
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getGoals,
+  setGoals,
+  updateGoals,
+  deleteGoals,
+} = require('../controllers/goalController');
 
-router.get('/', (req, res) => {
-  res.status(200).json({ message: 'Get Goals' });
-});
+router.get('/', getGoals);
 
-router.post('/', (req, res) => {
-  res.status(200).json({ message: 'Set Goal' });
-});
+router.post('/', setGoals);
 
-router.put('/:id', (req, res) => {
-  res.status(200).json({ message: `Update Goal ${req.params.id}` });
-});
+router.put('/:id', updateGoals);
 
-router.delete('/:id', (req, res) => {
-  res.status(200).json({ message: `Delete Goal ${req.params.id}` });
-});
+router.delete('/:id', deleteGoals);
 
 module.exports = router;
