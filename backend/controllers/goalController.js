f9d705a701d0e80f52e24112f7b9794b8e3b8618
Paul Thomas <paulthomas2107@gmail.com>
2022-12-05 20:15:14 +0000

    More...

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,12 @@
 const asyncHandler = require('express-async-handler');
+const Goal = require('../models/goalModel');
 
 // @desc    getGoal
 // @route   GET /api/goals
 // @access  Private
 const getGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get Goal' });
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
 
 // @desc    setGoal
@@ -15,8 +17,11 @@ const setGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('Please add a text field.');
   }
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
 
-  res.status(200).json({ message: 'Set Goal' });
+  res.status(200).json(goal);
 });
 
 // @desc    updateGoal
