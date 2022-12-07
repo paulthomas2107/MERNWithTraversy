ba58c76f83d5c2bee28274b94077c42fb07c00d4
Paul Thomas <paulthomas2107@gmail.com>
2022-12-07 09:46:18 +0000

    More....

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,26 @@
+// @desc    Register New User
+// @route   POST /api/users
+// @access  Public
 const registerUser = (req, res) => {
   res.json({ message: 'Register User' });
 };
 
+// @desc    Authenticate User
+// @route   POST /api/login
+// @access  Public
+const loginUser = (req, res) => {
+  res.json({ message: 'Login User' });
+};
+
+// @desc    Get User Data
+// @route   GET /api/users/me
+// @access  Public
+const getMe = (req, res) => {
+  res.json({ message: 'User Data Display' });
+};
+
 module.exports = {
   registerUser,
+  loginUser,
+  getMe,
 };
