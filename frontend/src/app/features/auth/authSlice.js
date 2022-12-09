bde2fa470175fadcf9bd6e30ba112b71129eb9d3
Paul Thomas <paulthomas2107@gmail.com>
2022-12-09 16:54:35 +0000

    More...

--- frontend/src/app/features/auth/authSlice.js
+++ frontend/src/app/features/auth/authSlice.js
@@ -12,6 +12,19 @@ const initialState = {
   message: '',
 };
 
+// Login user
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
+  try {
+    return await authService.login(user);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
 // Register User
 export const register = createAsyncThunk(
   'auth/register',
@@ -29,6 +42,10 @@ export const register = createAsyncThunk(
     }
   }
 );
+
+export const logout = createAsyncThunk('auth/logout', async () => {
+  await authService.logout();
+});
 
 export const authSlice = createSlice({
   name: 'auth',
@@ -55,6 +72,9 @@ export const authSlice = createSlice({
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(logout.fulfilled, (state) => {
         state.user = null;
       });
   },
