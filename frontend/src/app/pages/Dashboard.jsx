efb20876e7b397bbc2df0bf4e73592b8a7aad6ce
Paul Thomas <paulthomas2107@gmail.com>
2022-12-12 13:37:16 +0000

    Add goal function

--- frontend/src/app/pages/Dashboard.jsx
+++ frontend/src/app/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import GoalForm from '../../components/GoalForm';
 
 function Dashboard() {
   const navigate = useNavigate();
@@ -18,6 +19,7 @@ function Dashboard() {
         <h1>Welcome {user && user.name}</h1>
         <p>Goals Dashboard</p>
       </section>
+      <GoalForm />
     </>
   );
 }
