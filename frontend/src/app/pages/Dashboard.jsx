b09e236fbe88a52a270522de4e41f963660c972b
Paul Thomas <paulthomas2107@gmail.com>
2022-12-12 12:43:09 +0000

    More...

--- frontend/src/app/pages/Dashboard.jsx
+++ frontend/src/app/pages/Dashboard.jsx
@@ -12,7 +12,14 @@ function Dashboard() {
     }
   }, [user, navigate]);
 
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+    </>
+  );
 }
 
 export default Dashboard;
